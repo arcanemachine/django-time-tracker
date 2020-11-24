c8998908bee42f28c1f3470d72d87952c8364520
Nicholas Moen <arcanemachine@tutanota.com>
2020-11-24 02:32:53 -0700

    TimerSerializer: Show dates as Unix timestamps

--- api/serializers.py
+++ api/serializers.py
@@ -33,11 +33,20 @@ class TimerSerializer(serializers.ModelSerializer):
             'run_seconds', 'pause_seconds', 'is_running']
 
     def to_representation(self, instance):
+
+        def get_unix_time(time):
+            return int(time.timestamp()) if time else None
+
+        formatted_start_time = get_unix_time(self.instance.start_time)
+        formatted_stop_time = get_unix_time(self.instance.stop_time)
+        formatted_last_update_time = \
+            get_unix_time(self.instance.last_update_time)
+
         return {'id': instance.id,
                 'activity': instance.activity.pk,
-                'start_time': instance.start_time,
-                'stop_time': instance.stop_time,
-                'last_update_time': instance.last_update_time,
+                'start_time': formatted_start_time,
+                'stop_time': formatted_stop_time,
+                'last_update_time': formatted_last_update_time,
                 'run_seconds': instance.run_seconds,
                 'pause_seconds': instance.pause_seconds,
                 'is_running': instance.is_running}
